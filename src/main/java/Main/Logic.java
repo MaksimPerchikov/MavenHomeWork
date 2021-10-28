c8017a968fc13d1342684d37dbf38e4263818e77
MaksimchikPerchikov <maksim.perch999@mail.ru>
2021-10-28 20:00:34 +0300

    пытаюсь что-то сделать по первому ДЗ по Майвен

--- src/main/java/Main/Logic.java
+++ src/main/java/Main/Logic.java
@@ -1,23 +1,7 @@
 package Main;
 
-public class Main implements ShowString {
-
-    private String str;
-
-    Main(String str){
-        this.str = str;
-
-    }
-    public String getStr() {
-        return str;
-    }
-
-    public void setStr(String str) {
-        this.str = str;
-    }
-
-    @Override
-    public void methodShowString(Main main) {
-        System.out.println(main.getStr());
+public class Logic {
+    public String sayHello(){
+        return "Hello world";
     }
 }
--- src/main/java/Main/StartProject.java
+++ src/main/java/Main/StartProject.java
@@ -1,2 +1,8 @@
-package Main;public class StartProject {
+package Main;
+
+public class StartProject {
+    public static void main(String[] args) {
+        Logic logic= new Logic();
+        System.out.println(logic.sayHello());
+    }
 }
