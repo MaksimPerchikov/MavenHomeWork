39cfb67360211b7186d9355d582ddcffcdb3bd45
MaksimchikPerchikov <maksim.perch999@mail.ru>
2021-10-29 12:36:32 +0300

    1 дз по Майвен

--- src/main/java/ru/ibs/StartProject.java
+++ src/main/java/ru/ibs/StartProject.java
@@ -2,7 +2,6 @@ package Main;
 
 public class StartProject {
     public static void main(String[] args) {
-        Logic logic= new Logic();
-        System.out.println(logic.sayHello());
+        System.out.println("Hello World");
     }
 }
