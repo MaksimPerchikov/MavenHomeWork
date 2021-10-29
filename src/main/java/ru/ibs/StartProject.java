18da4ec7b7dac4742e3787dc3c9bbaa76e3d586f
MaksimchikPerchikov <maksim.perch999@mail.ru>
2021-10-29 12:36:46 +0300

    1.1 дз по Майвен

--- src/main/java/ru/ibs/StartProject.java
+++ src/main/java/ru/ibs/StartProject.java
@@ -1,7 +1,11 @@
-package Main;
+package ru.ibs;
+
+import java.util.Scanner;
 
 public class StartProject {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(String.format("Argument "+" "+i+". "+args[i]));
+        }
     }
 }
